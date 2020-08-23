ec900cfa56864f130c0917587e7c96721d6ee6d5
Vedant <vedantdesai07@gmail.com>
2020-08-22 22:48:26 -0400

    Added some comments for clarity

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 API_KEY = ''  # put your api Key id
-SECRET_KEY = ''
+SECRET_KEY = '' # Put your secret key
+# You can get these API and secret keys from alpaaca trading account
 HEADERS = {
     'APCA-API-KEY-ID': API_KEY,
     'APCA-API-SECRET-KEY': SECRET_KEY
 }
-
-BASE_URL = 'https://paper-api.alpaca.markets'
-
-Liquidate_all_the_positions_url = 'https://paper-api.alpaca.markets/v2/positions'
