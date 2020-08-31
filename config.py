bd5008580d62593df846dba6c0c656f8f0bbc39f
Vedant <vedantdesai07@gmail.com>
2020-08-31 12:05:52 -0400

    config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_KEY = ''  # put your api Key id
-SECRET_KEY = '' # Put your secret key
+SECRET_KEY = ''  # Put your secret key
 # You can get these API and secret keys from alpaaca trading account
 HEADERS = {
     'APCA-API-KEY-ID': API_KEY,
